d7de8c4dd38f308bc28127e09e713bfdf61ba3ae
celfi <celfimeysa@gmail.com>
2025-12-19 10:13:49 +0700

    YUP

---
+++ praktikum1PP.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string nama = "--celfi meysa ayuni--";
+  string NPM = "--2517051058--";
+    
+  cout << "Selamat Datang Di Praktikum Pengenalan Pemrograman" << endl;
+  cout << "nama saya adalah" << nama << endl;
+  cout << "NPM saya adalah" << NPM << endl << endl;
+  
+  cout << "unila indah" << endl;
+  cout << "be strong" << endl;
+}
